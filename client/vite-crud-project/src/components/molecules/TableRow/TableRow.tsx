25baefdce00e168f1cd1642dbe9defeeb611d615
antropofilija <alininyte1@gmail.com>
2023-07-12 20:11:23 +0300

    added change button logic

--- client/vite-crud-project/src/components/molecules/TableRow/TableRow.tsx
+++ client/vite-crud-project/src/components/molecules/TableRow/TableRow.tsx
@@ -14,6 +14,11 @@ interface IUser {
 const TableRow = () => {
   const [users, setUsers] = useState<IUser[]>([]);
   const [deletingUserId, setDeletingUserId] = useState<string | null>(null);
+  const [editingUserId, setEditingUserId] = useState<string | null>(null);
+  const [editedName, setEditedName] = useState('');
+  const [editedSurname, setEditedSurname] = useState('');
+  const [editedEmail, setEditedEmail] = useState('');
+  const [editedAge, setEditedAge] = useState('');
 
   useEffect(() => {
     fetchUsers();
@@ -44,8 +49,52 @@ const TableRow = () => {
     }
   };
 
-  const handleDismissDelete = () => {
+  const handleCancelDelete = () => {
     setDeletingUserId(null);
+  };
+
+  const handleEdit = (userId: string) => {
+    const user = users.find((user) => user._id === userId);
+    if (user) {
+      setEditingUserId(userId);
+      setEditedName(user.name);
+      setEditedSurname(user.surname);
+      setEditedEmail(user.email);
+      setEditedAge(user.age.toString());
+    }
+  };
+
+  const handleSave = async () => {
+    try {
+      if (editingUserId) {
+        const editedUser = {
+          name: editedName,
+          surname: editedSurname,
+          email: editedEmail,
+          age: parseInt(editedAge),
+        };
+        await axios.put(
+          `http://localhost:5000/api/crud/${editingUserId}`,
+          editedUser
+        );
+        await fetchUsers();
+        setEditingUserId(null);
+        setEditedName('');
+        setEditedSurname('');
+        setEditedEmail('');
+        setEditedAge('');
+      }
+    } catch (error) {
+      console.error(`Error editing user with ID ${editingUserId}:`, error);
+    }
+  };
+
+  const handleCancelEdit = () => {
+    setEditingUserId(null);
+    setEditedName('');
+    setEditedSurname('');
+    setEditedEmail('');
+    setEditedAge('');
   };
 
   return (
@@ -53,34 +102,70 @@ const TableRow = () => {
       <ul>
         {users.map((user) => (
           <StyledLi key={user._id}>
-            <p>{user.name}</p>
-            <p>{user.surname}</p>
-            <p>{user.email}</p>
-            <p>{user.age}</p>
-            {deletingUserId === user._id ? (
+            {editingUserId === user._id ? (
               <div>
-                <p>Ar tikrai norite ištrinti?</p>
+                <input
+                  type='text'
+                  value={editedName}
+                  onChange={(e) => setEditedName(e.target.value)}
+                />
+                <input
+                  type='text'
+                  value={editedSurname}
+                  onChange={(e) => setEditedSurname(e.target.value)}
+                />
+                <input
+                  type='email'
+                  value={editedEmail}
+                  onChange={(e) => setEditedEmail(e.target.value)}
+                />
+                <input
+                  type='number'
+                  value={editedAge}
+                  onChange={(e) => setEditedAge(e.target.value)}
+                />
                 <StyledConfirm>
-                  <Button onClick={handleConfirmDelete} styletype='add'>
-                    Taip
+                  <Button onClick={handleSave} styletype='add'>
+                    Išsaugoti
                   </Button>
-                  <Button onClick={handleDismissDelete} styletype='delete'>
+                  <Button onClick={handleCancelEdit} styletype='delete'>
                     Atšaukti
                   </Button>
                 </StyledConfirm>
               </div>
             ) : (
-              <StyledConfirm>
-                <Button onClick={() => handleDelete(user._id)} styletype='add'>
-                  Redaguoti
-                </Button>
-                <Button
-                  onClick={() => handleDelete(user._id)}
-                  styletype='delete'
-                >
-                  Ištrinti
-                </Button>
-              </StyledConfirm>
+              <div>
+                <p>{user.name}</p>
+                <p>{user.surname}</p>
+                <p>{user.email}</p>
+                <p>{user.age}</p>
+                {deletingUserId === user._id ? (
+                  <StyledConfirm>
+                    <p>Ar tikrai norite ištrinti?</p>
+                    <Button onClick={handleConfirmDelete} styletype='add'>
+                      Taip
+                    </Button>
+                    <Button onClick={handleCancelDelete} styletype='delete'>
+                      Atšaukti
+                    </Button>
+                  </StyledConfirm>
+                ) : (
+                  <StyledConfirm>
+                    <Button
+                      onClick={() => handleEdit(user._id)}
+                      styletype='add'
+                    >
+                      Redaguoti
+                    </Button>
+                    <Button
+                      onClick={() => handleDelete(user._id)}
+                      styletype='delete'
+                    >
+                      Ištrinti
+                    </Button>
+                  </StyledConfirm>
+                )}
+              </div>
             )}
           </StyledLi>
         ))}
